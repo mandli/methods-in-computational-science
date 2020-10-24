2fb1329ae591c6a08905d9e034ca69408a20ae7f
Kyle Mandli <kyle.mandli@gmail.com>
2020-10-23 22:34:15 -0400

    Add mpi C hello world

---
+++ src/C/mpi/hello_world.c
@@ -0,0 +1,26 @@
+
+// MPI Library
+#include "mpi.h"
+
+// Standard IO libraries
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[])
+{
+
+    int num_procs, rank;
+
+    // Initialize MPI
+    MPI_Init(&argc, &argv);
+
+    // Get total number of processes and this processes' rank
+    MPI_Comm_size(MPI_COMM_WORLD, &num_procs);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    printf("Hellow world from process %d of %d.\n", rank, num_procs);
+
+    MPI_Finalize();
+    
+    return 0;
+}
