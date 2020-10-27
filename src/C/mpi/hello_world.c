6e6077cca0e0192b640a62c1a056dca2e02e0081
Kyle Mandli <kyle.mandli@gmail.com>
2020-10-27 15:33:31 -0400

    Update all C MPI examples

--- src/C/mpi/hello_world.c
+++ src/C/mpi/hello_world.c
@@ -8,7 +8,6 @@
 
 int main(int argc, char* argv[])
 {
-
     int num_procs, rank;
 
     // Initialize MPI
---
+++ src/C/mpi/note_passing.c
@@ -0,0 +1,65 @@
+
+// MPI Library
+#include "mpi.h"
+
+// Standard IO libraries
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[])
+{
+    int num_procs, rank;
+    MPI_Status status;
+
+    int tag;
+    double message;
+
+    // Initialize MPI
+    MPI_Init(&argc, &argv);
+
+    // Get total number of processes and this processes' rank
+    MPI_Comm_size(MPI_COMM_WORLD, &num_procs);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    // If we only have one process then we are alone and cannot message anyone :(
+    if (num_procs == 1)
+    {
+        printf("Only one process used, no messages passed.\n");
+        MPI_Finalize();
+        return 0;
+    }
+
+    // Not super important for us
+    tag = 42;
+
+    // Start with rank 0 and start passing
+    if (rank == 0)
+    {
+        message = 2.718281828;
+        printf("Process %d sending message = %f\n", rank, message);
+
+        // We just send from the first rank
+        MPI_Send(&message, 1, MPI_DOUBLE_PRECISION, 1, tag, MPI_COMM_WORLD);
+    }
+    else if (rank < num_procs - 1)
+    {
+
+        // We first recieve and then send from these middle ranks
+        MPI_Recv(&message, 1, MPI_DOUBLE_PRECISION, rank-1, tag, MPI_COMM_WORLD, &status);
+        printf("Process %d recieved message = %f\n", rank, message);
+        printf("Process %d sending message = %f\n", rank, message);
+        MPI_Send(&message, 1, MPI_DOUBLE_PRECISION, rank+1, tag, MPI_COMM_WORLD);
+        
+    }
+    else if (rank == num_procs - 1)
+    {
+        // We just recieve in this last rank
+        MPI_Recv(&message, 1, MPI_DOUBLE_PRECISION, rank-1, tag, MPI_COMM_WORLD, &status);
+
+        printf("Process %d recieved message = %f\n", rank, message);
+    }
+
+    MPI_Finalize();
+    
+    return 0;
+}
