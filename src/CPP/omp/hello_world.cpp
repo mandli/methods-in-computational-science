ddf45da7d85f6ceb66f6b161a088e3dee1eeb248
Kyle Mandli <kyle.mandli@gmail.com>
2021-10-11 16:18:20 -0400

    Add basic c++ hello world example

---
+++ src/CPP/omp/hello_world.cpp
@@ -0,0 +1,25 @@
+
+// OpenMP library header
+#include <omp.h>
+
+// Standard io stream and namespace
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() 
+{
+    int total_threads = omp_get_num_threads();
+
+    // Fork into threads
+    #pragma omp parallel
+    {
+        int thread_ID = omp_get_thread_num();
+        // Concatenate the string and output as one operation to avoid race
+        // condition
+        string output = "Hello, World from " + to_string(thread_ID) + " of " + to_string(total_threads) + "!\n";
+        cout << output;
+    }
+
+    return 0;
+}
