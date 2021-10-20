de0832b3b30d8c21c88097406f9cb76bf15fa0e8
Kyle Mandli <kyle.mandli@gmail.com>
2021-10-20 14:19:53 -0400

    Add more complex hello world example

--- src/CPP/omp/hello_world.cpp
+++ src/CPP/omp/hello_world.cpp
@@ -9,12 +9,20 @@ using namespace std;
 
 int main() 
 {
-    int total_threads = omp_get_num_threads();
+    int total_threads;
 
     // Fork into threads
     #pragma omp parallel
     {
         int thread_ID = omp_get_thread_num();
+
+        if (thread_ID == 0)
+        {
+            total_threads = omp_get_num_threads();
+        }
+        #pragma omp barrier
+
+
         // Concatenate the string and output as one operation to avoid race
         // condition
         string output = "Hello, World from " + to_string(thread_ID) + " of " + to_string(total_threads) + "!\n";
