0b9858b12e253ff04adff83a676def53cb6f45a8
Kyle Mandli <kyle.mandli@gmail.com>
2021-11-08 14:14:06 -0500

    Add CPP compute pi examples

---
+++ src/CPP/mpi/compute_pi.cpp
@@ -0,0 +1,72 @@
+// MPI Library
+#include "mpi.h"
+
+// Standard IO libraries
+#include <iostream>
+using namespace std;
+
+#include <math.h>
+
+int main(int argc, char* argv[])
+{
+    int num_procs, rank, tag;
+    MPI_Status status;
+
+    double const pi = 3.1415926535897932384626433832795;
+    
+    int num_points, points_per_proc, start_index, end_index;
+    double x, dx, pi_sum, pi_sum_proc, difference;
+
+    // Initialize MPI
+    MPI_Init(&argc, &argv);
+
+    // Get total number of processes and this processes' rank
+    MPI_Comm_size(MPI_COMM_WORLD, &num_procs);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    // Rank 0 will ask for number of points
+    if (rank == 0)
+    {
+        cout << "How many points to use?\n";
+        cin >> num_points;
+    }
+    // Broadcast the number of points
+    MPI_Bcast(&num_points, 1, MPI_INTEGER, 0, MPI_COMM_WORLD);
+
+    dx = 1.0 / (double)num_points;
+
+    // Determine how many points to handle with each proc
+    points_per_proc = (num_points + num_procs - 1) / num_procs;
+    // Only print out the number of points per proc by rank 0
+    if (rank == 0)
+        cout << "Points per proc = " << points_per_proc << "\n.";
+
+    // Determine start and end indices for this rank's points
+    start_index = rank * points_per_proc;
+    end_index = (int)fmin((rank + 1) * points_per_proc, num_points) - 1;
+
+    // Diagnostic - Print the intervals handled by each rank
+    cout << "Rank " << rank << " - (" << start_index << ", " << end_index << ")\n";
+
+    pi_sum_proc = 0.0;
+    for (int i = start_index; i <= end_index; ++i)
+    {
+        x = (i - 0.5) * dx;
+        pi_sum_proc += 1.0 / (1.0 + pow(x, 2));
+    }
+
+    MPI_Reduce(&pi_sum_proc, &pi_sum, 1, MPI_DOUBLE_PRECISION, MPI_SUM, 0, 
+                MPI_COMM_WORLD);
+
+    if (rank == 0)
+    {
+        pi_sum *= 4.0 * dx;
+        difference = fabs(pi - pi_sum);
+        cout << "The approximation to pi is " << pi_sum << ".\n";
+        cout << "Difference = " << difference << ".\n";
+    }
+
+    MPI_Finalize();
+    
+    return 0;
+}
